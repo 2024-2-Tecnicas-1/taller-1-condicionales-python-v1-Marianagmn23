0dddec42badcc17d8a35ae2fa1143195356d6482
Mariana Alejandra Gordillo Meneses <marianagordillo1123@gmail.com>
2024-09-07 21:26:52 -0500

    Se adicionó el código del programa

--- src/triangulo.py
+++ src/triangulo.py
@@ -1,6 +1,18 @@
 def evaluar(a, b, c):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    respuesta = ""
+
+    if a < (b + c) and b < (a + c) and c < (a + b):
+        if (a == b and a != c) or (a == c and b != c) or (b == c and a != b):
+            respuesta = "El triángulo es isósceles"
+        elif a != b and b != c and a != c:
+            respuesta = "El triángulo es escaleno"
+        elif a == b and b == c:
+            respuesta = "El triángulo es equilátero"
+    else:
+        respuesta = "No es un triángulo válido"
+    
+    return respuesta
 
 if __name__ == '__main__':
     print("a:", end="")
