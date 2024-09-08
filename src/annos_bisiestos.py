7f30bb00a8b3f754dec480fec1557ad63880da52
Mariana Alejandra Gordillo Meneses <marianagordillo1123@gmail.com>
2024-09-07 21:30:07 -0500

    Se adicionó el código

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,15 +1,17 @@
 def evaluar(anno):
     # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
+    respuesta=""
     if anno >= 1582:
         if anno % 400 == 0 or (anno % 4 == 0 and anno % 100 != 0):
-            return f"{anno} es bisiesto"
+            respuesta = f"{anno} es bisiesto"
         else:
-            return f"{anno} no es bisiesto"
+            respuesta = f"{anno} no es bisiesto"
     else:
         if anno % 4 == 0:
-            return f"{anno} es bisiesto"
+            respuesta = f"{anno} es bisiesto"
         else:
-            return f"{anno} no es bisiesto"
+            respuesta = f"{anno} no es bisiesto"
+            return respuesta
             
 if __name__ == '__main__':
     print("Año:", end="")
