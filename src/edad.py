94c075c58d1407c7e05042cbb121034196382aa9
Mariana Alejandra Gordillo Meneses <marianagordillo1123@gmail.com>
2024-09-08 15:55:02 -0500

    Update edad.py

--- src/edad.py
+++ src/edad.py
@@ -1,18 +1,18 @@
-from datetime import datetime
+import datetime
+
 def evaluar(dia, mes, anno):
-    # TODO: Coloca aquí el código del ejercicio 6: Edad
-    current_date = datetime.now()
-    nacimiento = datetime(anno, mes, dia)
-
-    edad = current_date.year - nacimiento.year
-
-
-    if (current_date.month < nacimiento.month) or (current_date.month == nacimiento.month and current_date.day < nacimiento.day):
+    
+    fecha_actual = datetime.datetime.now()       
+    fecha_nacimiento = datetime.datetime(anno, mes, dia)    
+    edad = fecha_actual.year - fecha_nacimiento.year   
+    
+    if (fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
         edad -= 1
+         
+    respuesta= f"Tu edad es: {edad} años."
 
-    respuesta = "Usted tiene " + str(edad) + " años"
     return respuesta
-
+    
 if __name__ == '__main__':
     print("Ingrese su fecha de nacimiento")
     print("Día:", end="")
