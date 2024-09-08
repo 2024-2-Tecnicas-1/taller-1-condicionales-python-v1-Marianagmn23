67959e5479e4266f21303d5fd4ddf211f4bd8477
Mariana Alejandra Gordillo Meneses <marianagordillo1123@gmail.com>
2024-09-08 15:39:11 -0500

    Update annos_bisiestos.py

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,6 +1,5 @@
 def evaluar(anno):
     # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    respuesta=""
     if anno >= 1582:
         if anno % 400 == 0 or (anno % 4 == 0 and anno % 100 != 0):
             respuesta = f"{anno} es bisiesto"
